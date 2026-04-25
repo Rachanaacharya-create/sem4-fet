2bb28ccd7151973bad4b2a4ec61e83d3d55152d7
Rachana Acharya <rachanaacharya0485@gmail.com>
2026-04-25 10:29:16 +0530

    Fix typo in event listener method name

--- ASSIGNMENTS/one_script.js
+++ ASSIGNMENTS/one_script.js
@@ -2,7 +2,7 @@
     alert("Do you want to change the second line of the website?");
 
     const courseLine = document.getElementById("Student");
-    courseLine.addEventListner("Click",function(){
+    courseLine.addEventListner("Click", function() {
 
         let newText = prompt("Enter new text for the second line:");
 
